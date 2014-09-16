be4e35a5b280eb0f2ee7704a3518f47cde1637dc
Paul Cheng <bestpaul1985@gmail.com>
2014-09-16 14:01:04 -0400

    update prototype 5

---
+++ daisy_prototype_5/src/followerBig.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ofMain.h"
+#include "petal.h"
+#define PETALS_NUMBER 20
+class followerBig {
+    
+public:
+    
+    ofVec2f pos;
+    
+    ofVec2f vel;
+    ofVec2f frc;
+    
+    followerBig();
+    float map(float in, float inMin,float inMax, float outMin, float outMax, float shaper);
+
+    void setInitialCondition(ofImage &Daisy, ofImage &Petal, float px, float py, float vx, float vy);
+    void update();
+    void draw();
+    
+    void petalMove();
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    
+    void resetForce();
+    void addForce(float x, float y);
+    void addRepulsionForce(float x, float y, float radius, float scale);
+    void addAttractionForce(float x, float y, float radius, float scale);
+    void addRepulsionForce(followerBig &p, float radius, float scale);
+    void addAttractionForce(followerBig &p, float radius, float scale);
+    void addDampingForce();
+    void bounceOffWalls();
+    
+    float damping;
+    ofImage *daisyImage;
+    ofImage *petalImage;
+    vector <petal> myPetals;
+    float radius;
+    float angle;
+    float angleSpeed;
+    bool bPetalFixed;
+    bool isFirstBuncing;
+};
